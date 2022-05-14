d10eb41379593708d6e271f3d538a4a399fed0e5
Kicky Lau <94200416+kickylau@users.noreply.github.com>
2022-05-14 08:19:30 +0000

    first crud

--- app/forms/reservation_form.py
+++ app/forms/reservation_form.py
@@ -16,10 +16,10 @@ class NewReservation(FlaskForm):
 
 
 class EditReservation(FlaskForm):
-    taskType = SelectField("Task Type", choices=[(1,"Dog Walking"),(2,"Drop-In Visits")],validators=[InputRequired()])
-    taskLength = SelectField("Task Length", choices=[(1,"30 minutes"),(2,"60 minutes")],validators=[InputRequired()])
+    taskType = SelectField("Task Type", choices=["Dog Walking","Drop-In Visit"],validators=[InputRequired()])
+    taskLength = SelectField("Task Length", choices=["30 Minutes","60 Minutes"],validators=[InputRequired()])
     address = StringField("Address", validators=[DataRequired()])
     comment = StringField("Message", validators=[DataRequired()])
     date = DateField("Which Date?", validators=[DataRequired()])
-    time = SelectField("Between What Time?", choices=[(1,"6:00AM-9:00AM"),(2,"9:00AM-12:00PM"),(2,"12:00PM-3:00PM"),(3,"3:00PM-6:00PM"), (4,"6:00PM-9:00PM"), (5,"9:00PM-12:00AM")],validators=[InputRequired()])
+    time = SelectField("Between What Time?", choices=["6:00AM-9:00AM","9:00AM-12:00PM", "12:00PM-3:00PM", "3:00PM-6:00PM", "6:00PM-9:00PM","9:00PM-12:00AM"],validators=[InputRequired()])
     # submit = SubmitField("Submit")
